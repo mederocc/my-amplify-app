e15a8e3fa8e613c79c0e9ae230dfd4b26737d605
mederocc <mederocc@gmail.com>
2023-06-27 17:11:07 -0300

    resetting

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React from "react";
-// import AuthComponent from "./auth/Auth";
+import AuthComponent from "./auth/Auth";
 import Home from "./views/Home";
 import { Route, Routes } from "react-router-dom";
-import { configureAmplify } from "./current-config";
-import { Auth } from "aws-amplify";
-
-configureAmplify();
-
-console.log(Auth.configure());
 
 const App = () => {
-  async function signIn() {
-    try {
-      const user = await Auth.signIn("mederocc@gmail.com", "Calcifer91!");
-
-      console.log(user);
-    } catch (error) {
-      console.log("error signing in", error);
-    }
-  }
-
   return (
     <div>
-      <button onClick={signIn}>Sign in</button>
       <Routes>
-        {/* <Route path="/" element={<AuthComponent />} /> */}
+        <Route path="/" element={<AuthComponent />} />
         <Route exact path="/home" element={<Home />} />
       </Routes>
     </div>
